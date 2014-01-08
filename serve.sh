2da8cf896f8f21817573a21b66ff2d0b7d99c17c
Pontus Stenetorp <pontus@stenetorp.se>
2014-01-08 13:06:36 +0900

    Switch to Bundler and Kramdown

---
+++ serve.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install Bundler using:
+#
+#   sudo gem install bundler
+#   bundle install
+#
+# Late on, use the following to update:
+#
+#   bundle update
+
+# Note: I simply can't be bothered to remember this.
+bundle exec jekyll serve
